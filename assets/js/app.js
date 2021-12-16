ded29da5e10f1a77d3805cb7b8af777c49bd8da2
samiryakhlef <samir.yakhlef@gmail.com>
2021-12-16 16:05:50 +0100

    maj

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,7 +16,7 @@ function addParticipant(event) {
 
   //je créé un element li qui contient le prénom
   const participantElt = `
-<li>${participantName}</li>
+<li class="participant">${participantName}</li>
 `;
 
   //je récupère ul qui contient la list des participants
@@ -28,8 +28,76 @@ function addParticipant(event) {
   // je vide l'input
   nameInputElt.value = "";
 }
+
 // je récupère le formulaire d'ajout de nom
 const addNameFormElt = document.getElementById("addNameForm");
 
 //quand on soumet le formulaire, je veux que la fonction
 addNameFormElt.addEventListener("submit", addParticipant);
+
+function generateGroupe(participants, numberGroup) {
+  // je veux trier un tableau de nombre aleatoirement
+  //la fonction map prend un tableau en entrée
+  //applique une fonction sur chaque element du tableau
+  //et retourne un nouveau tableau
+  const sorted = participants
+    .map((participant) => ({ name: participant, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((participant) => participant.name);
+
+  // je veux générer des groupes de noms
+
+  const groupArr = [];
+  for (let i = 0; i < numberGroup; i++) {
+    groupArr.push([]);
+  }
+  var groupArrIndex = 0;
+  while (sorted.length > 0) {
+    groupArr[groupArrIndex].push(sorted.pop());
+    groupArrIndex++;
+    if (groupArrIndex >= groupArr.length) {
+      groupArrIndex = 0;
+    }
+  }
+
+  // afficher les groupes sur la page
+  const groupListElt = document.getElementById("groupList");
+
+  for (let groupList = 0; groupList < groupArr.length; groupList++) {
+    let groupElt = `
+    <div class="card bg-light mb-3" style="max-width: 20rem;">
+        <div class="card-header">Goupe${groupList + 1}</div>
+            <div class="card-body">
+            
+            <ul>`;
+    // Pour chaque groupe j'affiche la liste des des participant
+    for (
+      let participantList = 0;
+      participantList < groupArr[groupList].length;
+      participantList++
+    ) {
+      groupElt += `<li>${groupArr[groupList][participantList]}</li>`;
+    }
+
+    groupElt += `
+              </ul>
+          </div>
+      </div>
+      `;
+    groupListElt.innerHTML += groupElt;
+  }
+}
+const generateForm = document.getElementById("generateGroup");
+generateForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //récuperer le nombre de groupes
+  const numberGroup = parseInt(document.getElementById("groupNumber").value);
+  const participants = [];
+
+  // on veut récuperer la liste des paticipants
+  const participantElt = document.querySelectorAll(".participant");
+
+  const participantsElt = document.querySelectorAll(".participant");
+  participantsElt.forEach((element) => participants.push(element.textContent));
+  generateGroupe(participants, numberGroup);
+});
